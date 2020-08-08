3e8b19682f276d7720bee393026c58f208babe65
songtao <songtao@mit.edu>
2020-08-07 23:36:28 -0400

    update localserver

--- model/localserver.py
+++ model/localserver.py
@@ -9,10 +9,11 @@ import math
 import cv2
 import numpy as np 
 import tensorflow as tf 
+from time import time 
  
 from model import Sat2GraphModel
 from decoder import DecodeAndVis 
-
+from douglasPeucker import simpilfyGraph 
 
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.3)
 sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
@@ -23,8 +24,6 @@ model.restoreModel("../data/20citiesModel/model")
 gt_prob_placeholder = np.zeros((1,352,352,14))
 gt_vector_placeholder = np.zeros((1,352,352,12))
 gt_seg_placeholder = np.zeros((1,352,352,1))
-
-    
 
 
 
@@ -63,33 +62,78 @@ class S(BaseHTTPRequestHandler):
 		sat_img = scipy.ndimage.imread(input_file).astype(np.float)
 		max_v = 255
 		sat_img = (sat_img.astype(np.float)/ max_v - 0.5) * 0.9 
-		sat_img = sat_img.reshape((1,352,352,3))
+		sat_img = sat_img.reshape((1,704,704,3))
 
+		image_size = 352 
 
-		alloutputs  = model.Evaluate(sat_img, gt_prob_placeholder, gt_vector_placeholder, gt_seg_placeholder)
-		output = alloutputs[1][0,:,:,:]
+		weights = np.ones((image_size,image_size, 2+4*6 + 2)) * 0.001 
+		weights[32:image_size-32,32:image_size-32, :] = 0.5 
+		weights[56:image_size-56,56:image_size-56, :] = 1.0 
+		weights[88:image_size-88,88:image_size-88, :] = 1.5 
 
-		graph = DecodeAndVis(output, output_file, thr=0.05, snap=True, imagesize = 352)
+		mask = np.zeros((704, 704, 2+4*6 + 2))
+		output = np.zeros((704, 704, 2+4*6 + 2))
+
+		t0 = time()
+		for x in range(0,704-176-88,176/2):		
+			for y in range(0,704-176-88,176/2):
+
+				alloutputs  = model.Evaluate(sat_img[:,x:x+image_size, y:y+image_size,:], gt_prob_placeholder, gt_vector_placeholder, gt_seg_placeholder)
+				_output = alloutputs[1]
+
+				mask[x:x+image_size, y:y+image_size, :] += weights
+				output[x:x+image_size, y:y+image_size,:] += np.multiply(_output[0,:,:,:], weights)
+
+		print("GPU time:", time() - t0)
+		t0 = time()
+
+		output = np.divide(output, mask)
+
+		# alloutputs  = model.Evaluate(sat_img, gt_prob_placeholder, gt_vector_placeholder, gt_seg_placeholder)
+		# output = alloutputs[1][0,:,:,:]
+
+		graph = DecodeAndVis(output, output_file, thr=0.01, edge_thr = 0.1, angledistance_weight=50, snap=True, imagesize = 704)
+
+		print("Decode time:", time() - t0)
+		t0 = time()
+
+		graph = simpilfyGraph(graph)
+
+		print("Graph simpilfy time:", time() - t0)
+		t0 = time()
 
 		lines = []
 		points = []
 
-		
+		biasx = -102
+		biasy = -102
+
+		def addbias(loc):
+			return (loc[0]+biasx, loc[1]+biasy)
+
+		def inrange(loc):
+			if loc[0] > 102 and loc[0] < 602 and loc[1] > 102 and loc[1] < 602:
+				return True 
+			else:
+				return False 
 
 		for nid, nei in graph.iteritems():
 			for nn in nei:
-				edge = (nid, nn)
-				edge_ = (nn, nid)
-				if edge not in lines and edge_ not in lines:
-					lines.append(edge)  
+				if inrange(nn) or inrange(nid):
+					edge = (addbias(nid), addbias(nn))
+					edge_ = (addbias(nn), addbias(nid))
+					if edge not in lines and edge_ not in lines:
+						lines.append(edge)  
 
-			points.append(nid)
+			if inrange(nid) and len(nei)!=2:
+				points.append(addbias(nid))
 
 
 		# graph to json 
 
 		
 		return_str = json.dumps({"graph":[lines, points], "success":"true"})
+
 
 		# except:
 		# 	return_str = json.dumps({"success":"false"})
