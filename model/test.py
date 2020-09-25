247b773a15e4dfdeddbda9380e534a54b2996004
songtao <songtao@mit.edu>
2020-09-25 10:59:29 -0700

    sync

--- model/test.py
+++ model/test.py
@@ -18,62 +18,63 @@ import tifffile
 datafiles = json.load(open("train_prep_RE_18_20_CHN_KZN_250.json"))['data']
 basefolder = "/data/songtao/harvardDataset5m/"
 
-prefix = "4559325_2019-07-03_RE4_3A_"
+prefixs = ["4559325_2019-07-03_RE4_3A_", "4459815_2019-10-13_RE1_3A_", "4659204_2019-07-03_RE4_3A_", "4459815_2018-07-07_RE2_3A_", "4559216_2018-07-20_RE2_3A_", "4559325_2019-08-24_RE3_3A_", "4559325_2019-09-28_RE1_3A_"]
+for prefix in prefixs:
 
-input_img = np.zeros((5120,5120,5))
-input_mask = np.zeros((5120, 5120))
+	input_img = np.zeros((5120,5120,5))
+	input_mask = np.zeros((5120, 5120))
 
-for item in datafiles:
-	if prefix in item[1]:
-		#print(item)
+	for item in datafiles:
+		if prefix in item[1]:
+			#print(item)
 
-		items = item[1].split("Analytic_")
-		xy = items[-1].split("_")[0].split("-")
-		x = int(xy[1]) + 60
-		y = int(xy[0]) + 60 
+			items = item[1].split("Analytic_")
+			xy = items[-1].split("_")[0].split("-")
+			x = int(xy[1]) + 60
+			y = int(xy[0]) + 60 
 
-		file = basefolder + "/" + item[1]
+			file = basefolder + "/" + item[1]
 
-		img = tifffile.imread(file)
-		sat_img = img[:,:,0:5].astype(np.float)/(16384) - 0.5 
+			img = tifffile.imread(file)
+			sat_img = img[:,:,0:5].astype(np.float)/(16384) - 0.5 
 
-		input_img[x:x+250, y:y+250, :] = sat_img
-		input_mask[x:x+250, y:y+250] = 1.0 
-
-
-Image.fromarray(((input_img[:,:,0:3]+0.5) * 255.0).astype(np.uint8)).save("output/"+prefix+"rgb.png")
+			input_img[x:x+250, y:y+250, :] = sat_img
+			input_mask[x:x+250, y:y+250] = 1.0 
 
 
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
-with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-	model = Sat2GraphModel(sess, image_size=256, image_ch=5, resnet_step = 8, batchsize = 1, channel = 12, mode = "test")
-	model.restoreModel(sys.argv[1])
-
-	output = np.zeros((5120,5120,26))
-	weights = np.zeros((5120,5120,26))+0.00001
-	localweights = np.zeros((256,256,26)) + 0.00001 
-	localweights[32:224,32:224,:] = 0.5
-	localweights[64:192,64:192,:] = 1.0 
+	Image.fromarray(((input_img[:,:,0:3]+0.5) * 255.0).astype(np.uint8)).save("output/"+prefix+"rgb.png")
 
 
-	gt_prob = np.zeros((1,256,256,14))
-	gt_vector = np.zeros((1,256,256,12))
-	gt_seg = np.zeros((1,256,256,1))
+	gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
+	with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+		model = Sat2GraphModel(sess, image_size=256, image_ch=5, resnet_step = 8, batchsize = 1, channel = 12, mode = "test")
+		model.restoreModel(sys.argv[1])
 
-	for x in range(0, 5120-192, 64):
-		print(x)
-		for y in range(0,5120-192,64):
-			if np.sum(input_mask[x:x+256,y:y+256]) > 10.0:
-				_, output_ = model.Evaluate(input_img[x:x+256,y:y+256,:].reshape((1,256,256,5)), gt_prob, gt_vector, gt_seg)
+		output = np.zeros((5120,5120,26))
+		weights = np.zeros((5120,5120,26))+0.00001
+		localweights = np.zeros((256,256,26)) + 0.00001 
+		localweights[32:224,32:224,:] = 0.5
+		localweights[64:192,64:192,:] = 1.0 
 
-				output[x:x+256,y:y+256,:] += output_[0,:,:,0:26] * localweights
-				weights[x:x+256,y:y+256,:] += localweights
 
-	output = np.divide(output, weights)
+		gt_prob = np.zeros((1,256,256,14))
+		gt_vector = np.zeros((1,256,256,12))
+		gt_seg = np.zeros((1,256,256,1))
 
-	DecodeAndVis(output, "output/"+prefix+"output", thr=0.01, edge_thr=0.01, snap=True, imagesize = 5120)
+		for x in range(0, 5120-192, 64):
+			print(x)
+			for y in range(0,5120-192,64):
+				if np.sum(input_mask[x:x+256,y:y+256]) > 10.0:
+					_, output_ = model.Evaluate(input_img[x:x+256,y:y+256,:].reshape((1,256,256,5)), gt_prob, gt_vector, gt_seg)
 
-							
+					output[x:x+256,y:y+256,:] += output_[0,:,:,0:26] * localweights
+					weights[x:x+256,y:y+256,:] += localweights
+
+		output = np.divide(output, weights)
+
+		DecodeAndVis(output, "output/"+prefix+"output", thr=0.01, edge_thr=0.01, snap=True, imagesize = 5120)
+
+								
 
 
 
