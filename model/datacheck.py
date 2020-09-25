97eed40b46f1bdc77ec8a575bf171741c0fc4e58
songtao <songtao@mit.edu>
2020-09-25 09:28:19 -0700

    sync

--- model/datacheck.py
+++ model/datacheck.py
@@ -7,6 +7,8 @@ basefolder = "/data/songtao/harvardDataset5m/"
 
 trainfiles = []
 validfiles = []
+testfiles = []
+
 bad = 0
 for item in datafiles:
 	if item[-1] == 'train':
@@ -21,7 +23,20 @@ for item in datafiles:
 			bad+= 1
 
 		validfiles.append(basefolder+item[1].replace(".tif",""))
+	elif item[-1] == 'test':
+		filepath = basefolder+item[1]
+		if os.path.isfile(filepath):
+			bad+= 1
+
+		testfiles.append(basefolder+item[1].replace(".tif",""))
+
+testfiles = sorted(testfiles)
+print(testfiles)
+
+
 
 print("train size", len(trainfiles))
 print("valid size", len(validfiles))
 print("bad files", bad)
+
+
