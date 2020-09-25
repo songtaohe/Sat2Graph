a78c6186e49b54621f174547aa7b838d9fefdf2a
songtao <songtao@mit.edu>
2020-09-25 10:23:22 -0700

    sync

--- model/test.py
+++ model/test.py
@@ -20,8 +20,8 @@ basefolder = "/data/songtao/harvardDataset5m/"
 
 prefix = "4559325_2019-07-03_RE4_3A_"
 
-input_img = np.zeros((5000,5000,5))
-
+input_img = np.zeros((5120,5120,5))
+input_mask = np.zeros((5120, 5120))
 
 for item in datafiles:
 	if prefix in item[1]:
@@ -29,8 +29,8 @@ for item in datafiles:
 
 		items = item[1].split("Analytic_")
 		xy = items[-1].split("_")[0].split("-")
-		x = int(xy[1])
-		y = int(xy[0])
+		x = int(xy[1]) + 60
+		y = int(xy[0]) + 60 
 
 		file = basefolder + "/" + item[1]
 
@@ -38,16 +38,41 @@ for item in datafiles:
 		sat_img = img[:,:,0:5].astype(np.float)/(16384) - 0.5 
 
 		input_img[x:x+250, y:y+250, :] = sat_img
+		input_mask[x:x+250, y:y+250] = 1.0 
 
 
-Image.fromarray(((input_img[:,:,0:3]+0.5) * 255.0).astype(np.uint8)).save(prefix+"rgb.png")
+Image.fromarray(((input_img[:,:,0:3]+0.5) * 255.0).astype(np.uint8)).save("output/"+prefix+"rgb.png")
+
+
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
+with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+	model = Sat2GraphModel(sess, image_size=image_size, image_ch=5, resnet_step = 8, batchsize = 1, channel = 12, mode = "test")
+	model.restoreModel(sys.argv[1])
+
+	output = np.zeros((5120,5120,26))
+	weights = np.zeros((5120,5120))+0.00001
+
+	gt_prob = np.zeros_like((1,256,256,14))
+	gt_vector = np.zeros_like((1,256,256,12))
+	gt_seg = np.zeros_like((1,256,256,1))
+
+	for x in range(0, 5120-256, 128):
+		for y in range(0,5120-256,128):
+			if np.sum(input_mask[x:x+256,y:y+256]) > 10.0:
+				_, output_ = model.Evaluate(input_img[x:x+256,y:y+256,:].reshape((1,256,256,5)), gt_prob, gt_vector, gt_seg)
+
+				output[x:x+256,y:y+256,:] += output_[0,:,:,:]
+				weights[x:x+256,y:y+256,:] += 1.0 
+
+	output = np.divide(output, weights)
+
+	DecodeAndVis(output, "output/"+prefix+"output", thr=0.05, snap=True, imagesize = 5120)
+
+							
 
 
 
 
 
-# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
-# with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-# 	model = Sat2GraphModel(sess, image_size=image_size, image_ch=5, resnet_step = args.resnet_step, batchsize = batch_size, channel = args.channel, mode = args.mode)
-# 	model.restoreModel(sys.argv[1])
+
 
