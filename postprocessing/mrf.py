2f3099a3ddd24e66280b59c9a537a229bdab0670
songtao <songtao@mit.edu>
2020-11-19 11:41:08 -0800

    sync

--- postprocessing/mrf.py
+++ postprocessing/mrf.py
@@ -5,7 +5,7 @@ import sys
 import opengm
 from common import neighbors_cos 
 
-weight = 10.0 
+weight = 100.0 
 
 nodeneighbor = pickle.load(open(sys.argv[1]))
 edgeClass = pickle.load(open(sys.argv[2]))
