ef154b0de00e8d19b04f88005d9da0985f29e7ef
songtao <songtao@mit.edu>
2020-11-20 20:09:42 -0800

    add skysat2satgraph.py

--- prepare_dataset/SkySat2Sat2Graph.py
+++ prepare_dataset/SkySat2Sat2Graph.py
@@ -23,18 +23,18 @@ for rid in range(len(regionname)):
 	if rid % 100 == 0:
 		print(rid)
 	# convert image to png 
-	img = tifffile.imread(regionname[rid]+".tif")
+	img = tifffile.imread(skysat_folder+"Sat2Graph_Satellite_Images/"+ regionname[rid]+".tif")
 	rgb = img[:,:,0:3]
 	Image.fromarray(rgb).save(sat2graph_folder+"/region_%d_sat.png" % rid)
 
 
 	# interpolate graph to 20-pixel interval
-	graph = pickle.load(open(regionname[rid] + "_refine_gt_graph.p"))
+	graph = pickle.load(open(skysat_folder+"Sat2Graph_Graphs/"+regionname[rid] + "_refine_gt_graph.p"))
 	graph = graph_ops.graphDensify(graph, density=20, distFunc=graph_ops.PixelDistance)
 	pickle.dump(graph, open(sat2graph_folder+"/region_%d_refine_gt_graph.p" % rid, "w"))
 
 
 	# copy other files 
-	Popen("cp "+regionname[rid] + "_gt.png "+ sat2graph_folder+"/region_%d_refine_gt_graph.p" % rid, shell=True)
-	Popen("cp "+regionname[rid] + "_gt_graph_samplepoints.json "+ sat2graph_folder+"/region_%d_gt_graph_samplepoints.json" % rid, shell=True)
+	Popen("cp "+skysat_folder+"Sat2Graph_Graphs/"+regionname[rid] + "_gt.png "+ sat2graph_folder+"/region_%d_refine_gt_graph.p" % rid, shell=True)
+	Popen("cp "+skysat_folder+"Sat2Graph_Graphs/"+regionname[rid] + "_gt_graph_samplepoints.json "+ sat2graph_folder+"/region_%d_gt_graph_samplepoints.json" % rid, shell=True)
 
