34d0fb15920fda80c6329535c44bc3861d6450ff
songtao <songtao@mit.edu>
2020-11-22 19:07:33 -0800

    sync

--- model/tensorflowTest.py
+++ model/tensorflowTest.py
@@ -15,7 +15,7 @@ with tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions())) as sess:
         assign_ops.append(variables[-1].assign(input_placeholders[-1]))
     
         # create some random arrays
-        arrays.append(np.random.rand((3,3,tensor_size, tensor_size)))
+        arrays.append(np.random.rand(3,3,tensor_size, tensor_size))
 
     # assign the random array to the tf variables
     sess.run(assign_ops, feed_dict = {i:a for i,a in zip(input_placeholders, arrays)})
