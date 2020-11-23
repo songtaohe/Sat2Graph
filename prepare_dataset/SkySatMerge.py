38475f287a5b9a0337c7a6b75536d022bd4da80e
songtao <songtao@mit.edu>
2020-11-22 22:14:26 -0800

    sync

--- prepare_dataset/SkySatMerge.py
+++ prepare_dataset/SkySatMerge.py
@@ -13,6 +13,10 @@ import cv2
 skysat_folder = "../../data/SkySat_Sat2Graph/"
 output_folder = "../../data/SkySatRegions/"
 
+
+skysat_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
 Popen("mkdir -p "+output_folder, shell=True).wait()
 
 tilename = []
@@ -32,35 +36,31 @@ for i in range(len(regionname)):
     regionimg = np.zeros((4096, 4096,3), dtype=np.uint8)
     gtimg = np.zeros((4096, 4096), dtype=np.uint8)
     try:
-        # regionimg[0:2048,0:2048] = tifffile.imread(skysat_folder+"Sat2Graph_Satellite_Images/"+ regionname[i]+ "_0.tif")[:,:,0:3] 
-        # regionimg[2048:4096,0:2048] = tifffile.imread(skysat_folder+"Sat2Graph_Satellite_Images/"+ regionname[i]+ "_1.tif")[:,:,0:3] 
-        # regionimg[0:2048,2048:4096] = tifffile.imread(skysat_folder+"Sat2Graph_Satellite_Images/"+ regionname[i]+ "_2.tif")[:,:,0:3] 
-        # regionimg[2048:4096,2048:4096] = tifffile.imread(skysat_folder+"Sat2Graph_Satellite_Images/"+ regionname[i]+ "_3.tif")[:,:,0:3] 
+        regionimg[0:2048,0:2048] = tifffile.imread(skysat_folder+"Sat2Graph_Satellite_Images/"+ regionname[i]+ "_0.tif")[:,:,0:3] 
+        regionimg[2048:4096,0:2048] = tifffile.imread(skysat_folder+"Sat2Graph_Satellite_Images/"+ regionname[i]+ "_1.tif")[:,:,0:3] 
+        regionimg[0:2048,2048:4096] = tifffile.imread(skysat_folder+"Sat2Graph_Satellite_Images/"+ regionname[i]+ "_2.tif")[:,:,0:3] 
+        regionimg[2048:4096,2048:4096] = tifffile.imread(skysat_folder+"Sat2Graph_Satellite_Images/"+ regionname[i]+ "_3.tif")[:,:,0:3] 
         
-        gtimg[0:2048,0:2048] = nd.imread(skysat_folder+"Sat2Graph_Graphs/"+ regionname[i]+ "_0_gt.png")[:,:] 
-        gtimg[2048:4096,0:2048] = nd.imread(skysat_folder+"Sat2Graph_Graphs/"+ regionname[i]+ "_1_gt.png")[:,:] 
-        gtimg[0:2048,2048:4096] = nd.imread(skysat_folder+"Sat2Graph_Graphs/"+ regionname[i]+ "_2_gt.png")[:,:] 
-        gtimg[2048:4096,2048:4096] = nd.imread(skysat_folder+"Sat2Graph_Graphs/"+ regionname[i]+ "_3_gt.png")[:,:] 
+        # gtimg[0:2048,0:2048] = nd.imread(skysat_folder+"Sat2Graph_Graphs/"+ regionname[i]+ "_0_gt.png")[:,:] 
+        # gtimg[2048:4096,0:2048] = nd.imread(skysat_folder+"Sat2Graph_Graphs/"+ regionname[i]+ "_1_gt.png")[:,:] 
+        # gtimg[0:2048,2048:4096] = nd.imread(skysat_folder+"Sat2Graph_Graphs/"+ regionname[i]+ "_2_gt.png")[:,:] 
+        # gtimg[2048:4096,2048:4096] = nd.imread(skysat_folder+"Sat2Graph_Graphs/"+ regionname[i]+ "_3_gt.png")[:,:] 
         
     except:
         print("wrong...", i, regionname[i] )
 
-    Image.fromarray(gtimg).save(output_folder+"/" + regionname[i]+ "_gt.png")
+    # Image.fromarray(gtimg).save(output_folder+"/" + regionname[i]+ "_gt.png")
 
-    sat = nd.imread(output_folder+"/" + regionname[i]+ ".png").astype(np.float)
-    sat[:,:,0] += gtimg
-    sat[:,:,0] = np.clip(sat[:,:,0], 0, 255)
-    sat = sat.astype(np.uint8)
+    # sat = nd.imread(output_folder+"/" + regionname[i]+ ".png").astype(np.float)
+    # sat[:,:,0] += gtimg
+    # sat[:,:,0] = np.clip(sat[:,:,0], 0, 255)
+    # sat = sat.astype(np.uint8)
 
-    Image.fromarray(sat).save(output_folder+"/" + regionname[i]+ "_gt_sat.png")
+    # Image.fromarray(sat).save(output_folder+"/" + regionname[i]+ "_gt_sat.png")
 
-
-
-    
-
-    # Image.fromarray(regionimg).save(output_folder+"/" + regionname[i]+ ".png")
-    # regionimg = scipy.misc.imresize(regionimg, (2458, 2458))
-    # Image.fromarray(regionimg).save(output_folder+"/" + regionname[i]+ "_1m.png")
+    Image.fromarray(regionimg).save(output_folder+"/" + regionname[i]+ ".png")
+    regionimg = scipy.misc.imresize(regionimg, (2458, 2458))
+    Image.fromarray(regionimg).save(output_folder+"/" + regionname[i]+ "_1m.png")
 
 
 
