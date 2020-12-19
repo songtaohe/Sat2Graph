068c72494a4f074591e0043df6b4e288d0ea94e9
songtao <songtao@mit.edu>
2020-12-18 21:24:39 -0800

    sync

--- model/localserver.py
+++ model/localserver.py
@@ -20,7 +20,7 @@ global_lock = threading.Lock()
 
 #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.65)
 gpu_options = tf.GPUOptions(allow_growth=True)
-sess = tf.Session(config=tf.ConfigProto()) # gpu_options=gpu_options
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) # gpu_options=gpu_options
 
 #model = Sat2GraphModel(sess, image_size=352, resnet_step = 8, batchsize = 1, channel = 24, mode = "test", model_name="unet")
 #model.restoreModel("/data/songtao/Sat2GraphLib/globalmodel20200804v3UNET_352_8__channel24/model600000")
