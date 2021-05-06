0f8c9a90740616dfa32acffcfdedc6eb99059a7d
songtao <songtao@mit.edu>
2021-05-05 22:31:36 -0700

    fix an implementation bug in APLS; the correct APLS gives much higher scores in the paper

--- metrics/apls/main.go
+++ metrics/apls/main.go
@@ -494,18 +494,18 @@ func apls_one_way(graph_gt *graph, graph_prop *graph, ret chan float64) {
 			pair_num += 1 
 
 			
-
-			// if cp1_prop == -1 || cp2_prop == -1 {
-			// 	fmt.Println(shortest_paths_prop[cp1_prop][cp2_prop])
-			// }
-
-			if cp1_prop == -1 || cp2_prop == -1 {
+			// There was a bug here!!!!
+			// We should first check if there is a path on the ground truth map! 
+			// If there is no path on the ground truth map, we should not penalize the recall!!! 
+			// After fixing this bug, the apls should be much higher!
+			d1 := shortest_paths_gt[cp1_gt][cp2_gt]
+			if (cp1_prop == -1 || cp2_prop == -1) && (d1 > min_distance_filter) {
 				cc += 1.0
 				sum += 1.0
 				continue 
 			} 
 
-			d1 := shortest_paths_gt[cp1_gt][cp2_gt]
+			//d1 := shortest_paths_gt[cp1_gt][cp2_gt]
 			if d1 > min_distance_filter {
 				d2 := shortest_paths_prop[cp1_prop][cp2_prop]
 
